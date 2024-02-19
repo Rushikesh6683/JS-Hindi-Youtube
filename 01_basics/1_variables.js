9a21c7d963704e5d21ed9812db68406cc22d9716
Rushikesh6683 <160482135+Rushikesh6683@users.noreply.github.com>
2024-02-19 15:20:58 +0000

    Variable Update 1

---
+++ 01_basics/1_variables.js
@@ -0,0 +1,14 @@
+
+const accountId =1234                            // Cannotbe changed
+let accountName="Rushikesh Thorave"              // Can be change
+var accountEmail="rushit.2628@gmail.com"         // Can be change
+
+let accountState;   // undefined
+
+console.table([accountId,accountName,accountEmail,acountCity])
+
+/*  NOTE:-
+    Prefer NOT to use var
+    because of issue in block scope and functional scope
+*/
+
