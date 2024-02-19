7cf95fb7210b4aeb0bd6ae1cc4d61b8a3843c02d
Rushikesh6683 <160482135+Rushikesh6683@users.noreply.github.com>
2024-02-19 15:43:21 +0000

    Datatypes added

---
+++ 01_basics/2_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS code as newer version
+
+console.log(typeof null)      // object 
+console.log(typeof undefined) // undefined
+
+/*  
+    Notes:
+    
+    DataTypes
+1. Number  : 2 to power 53
+2. Bigint  : stocks,trading etc
+3. String  : prefer in ""
+4. Boolean : true/false
+5. Null    : standalone value   >> It is itself and Object
+6. Undefined :
+7. Symbol  : unique
+*/
