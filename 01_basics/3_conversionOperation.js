9808f734b4a6dbb03d0b61d8129bd5efc8099e38
Rushikesh6683 <160482135+Rushikesh6683@users.noreply.github.com>
2024-02-21 06:12:11 +0000

    updated

--- 01_basics/3_conversionOperation.js
+++ 01_basics/3_conversionOperation.js
@@ -3,3 +3,16 @@ console.log(typeof(score))
 
 let scoreNumber=Number(score)
 console.log(typeof(scoreNumber))
+//------------------
+
+let loggedIn= 1
+let booleanLoggedIn= Boolean(loggedIn)
+console.log(booleanLoggedIn)
+
+/* Notes:
+    "33"    >> 33
+    "33abc" >> NaN (Not a number)
+    trues   >> 1
+    false   >> 0
+
+*/
